26982d2467b6e44b504eb08963a179f16557e260
anish-107 <anish.k7644@gmail.com>
2025-08-10 01:03:14 +0530

    Updated favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,14 +6,23 @@ import Footer from "../components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "NAMYAM is Coming Soon!",
+  title: "NAMYAM",
   description: "Your ultimate online marketplace for authentic Puja Samagri Kits and spiritual products.",
+  icons: {
+    icon: [
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon.png", type: "image/png" },
+      { url: "/favicon.ico", type: "image/x-icon" }, // optional legacy
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gradient-to-b from-[#fffaf0] to-[#fdf5e6] dark:from-gray-900 dark:to-black text-gray-800 dark:text-gray-200 flex flex-col min-h-screen`}>
+      <body
+        className={`${inter.className} bg-gradient-to-b from-[#fffaf0] to-[#fdf5e6] dark:from-gray-900 dark:to-black text-gray-800 dark:text-gray-200 flex flex-col min-h-screen`}
+      >
         <Header />
         <main className="flex-grow flex items-center justify-center py-12 px-4">
           {children}
